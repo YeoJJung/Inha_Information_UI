93a7a3eaa808f41bc6a61f3987e2d7b4b67ba308
SiU <swear428@naver.com>
2022-12-07 22:44:01 +0900

    js안되요

--- 김시유팀플/script.js
+++ 김시유팀플/script.js
@@ -1,5 +1,5 @@
 function ViewLayer() {
-  document.getElementById("Pop_c").style.display = "inline";
+  document.getElementsByClassName("Pop_c").style.display = "inline";
 }
 function ViewLayer() {
     document.getElementById("Pop_d").style.display = "inline";
