2026ae5f3be4de47b52de61eb93c110d4028ff63
SiU <swear428@naver.com>
2022-12-09 14:33:12 +0900

    미완성

--- 김시유팀플/script.js
+++ 김시유팀플/script.js
@@ -1,6 +1,15 @@
+// function ViewLayer() {
+//   document.getElementsByClassName('Pop_c').style.display = "inline";
+// }
+// function ViewLayer() {
+//     document.getElementById("Pop_d").style.display = "inline";
+//   }
+
 function ViewLayer() {
-  document.getElementsByClassName("Pop_c").style.display = "inline";
-}
-function ViewLayer() {
-    document.getElementById("Pop_d").style.display = "inline";
+  var con = document.getElementById("pop_c");
+  if (con.style.display == "none") {
+    con.style.display = "block";
+  } else {
+    con.style.display = "none";
   }
+}
