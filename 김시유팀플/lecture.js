7cea7756e0628e5720be5f0955e3eeaa3f153aaf
ImxYJL <allensain14@gmail.com>
2022-12-27 15:38:38 +0900

    주말 정보 안뜨게 수정

--- 김시유팀플/lecture.js
+++ 김시유팀플/lecture.js
@@ -1,39 +1,44 @@
 var time;
 var date;
 var dayOfWeek;
+var isWeekend = false;
 
+// <동적으로 강의실 사용 현황을 출력할 html 영역을 지정>
+const target = document.getElementById("timetableList");
 
-// <선택한 날짜가 주말인지 아닌지 판정하는 함수>
-function getDayOfWeek(date){ 
-    const week = ['일', '월', '화', '수', '목', '금', '토'];
-    dayOfWeek = week[new Date(date).getDay()];
-    //원본은 const형 변수로 받았음 
-    return dayOfWeek;
-
-}
 // <초기 날짜 세팅: 오늘 날짜로>
-document.getElementById("checkedDate").valueAsDate = new Date();
+date = document.getElementById("checkedDate").valueAsDate = new Date();
+
+// <timetable 디폴트 설정: 보이지 않음> 
+document.getElementById("timetable").style.display = 'none';
 
 // <선택한 시간 저장>
 const timeOptionEls = document.getElementById("time"); 
+
+// <선택한 날짜 저장>
+const dateOptionEl = document.getElementById("checkedDate");
+
+// <선택한 날짜가 무슨 요일인지>
+function getDayOfWeek(date){ 
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    dayOfWeek = week[new Date(date).getDay()];
+    return dayOfWeek;
+}
+
 const timeClickEvent = () =>{
     time = timeOptionEls.options[timeOptionEls.selectedIndex].value;
 }
 timeOptionEls.addEventListener("onchange", timeClickEvent);
 
-// <선택한 날짜 저장>
-const dateOptionEl = document.getElementById("checkedDate");
 const dateClickEvent = () =>{
     date = dateOptionEl.value;
 
     //만약 주말을 선택했다면 정보가 없다는 알림을 띄움
-    if(getDayOfWeek(date) ==="토" || getDayOfWeek(date) ==="일" ){
+    if(getDayOfWeek(date) ==="토" || getDayOfWeek(date) ==="일"){
+        isWeekend = true;
         alert("정보가 존재하지 않습니다.");
-    }
+    }else isWeekend = false;
 }
-
-// <timetable 디폴트 설정: 보이지 않음> 
-document.getElementById("timetable").style.display = 'none';
 
 // <난수를 생성하는 함수: 난수의 배수인 교시를 사용 처리함>
 function setRandom(){
@@ -43,7 +48,6 @@ function setRandom(){
 
 //<조회 버튼 다시 눌렀을 때 기존 시간표 정보를 보이지 않게 하기 위해 삭제>
 function deleteTimetable(){
-    const target = document.getElementById("timetableList");
     while(target.firstChild){
         target.removeChild(target.firstChild);
     }
@@ -52,15 +56,14 @@ function deleteTimetable(){
 //<조회 버튼 눌렀을 때 강의실 정보 표시>
 function setTimetable(){
     deleteTimetable();
+    if(isWeekend){ alert("정보가 존재하지 않습니다."); return;}
 
     var time = timeOptionEls.options[timeOptionEls.selectedIndex].value;
     var date = dateOptionEl.value;
     var random = setRandom();
 
-    const target = document.getElementById("timetableList");
-
     for(var i = 0; i<20; i++){
-         let listEl = document.createElement("li");
+        let listEl = document.createElement("li");
         if((i+1) % random === 0){
             listEl.innerHTML = `<p>60주년 - 0${i+1} 강의실 ${time}교시 
             <span style='color:red'> 사용 </span> <span> (${date})</span> </p>`;
